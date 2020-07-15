d4700b42cbed83afd8112c0d4525f38b43d3f956
jaikrish.dxj@gmail.com <Sjaikrish.Jaikrish@pb.com>
2020-07-15 14:36:03 +0530

    first commit

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request,jsonify
-# from flask_cors import CORS,cross_origin
+from flask_cors import CORS, cross_origin
 import requests
 import pymongo
+import os
 
 from bs4 import BeautifulSoup
 from urllib.request import urlopen as uReq
@@ -9,14 +10,16 @@ from bs4 import BeautifulSoup
 import requests
 
 
-app = Flask(__name__) 
+app = Flask(__name__)
+CORS(app) 
 
-dbConn = pymongo.MongoClient("mongodb://localhost:27017/")
+#dbConn = pymongo.MongoClient("mongodb://localhost:27017/")
 
-dbname='crawlerDB'
-db = dbConn[dbname]
+#dbname='crawlerDB'
+#db = dbConn[dbname]
 
 @app.route('/', methods=['GET', 'POST']) # To render Homepage
+@cross_origin()
 def index():
     if (request.method == 'POST'):
         
@@ -24,39 +27,39 @@ def index():
         searchString = request.form['content'].replace(" ","")
 
         
-        collection=db[searchString]
-        reviews = collection.find({})
+        # collection=db[searchString]
+        # reviews = collection.find({})
 
-        if reviews.count() > 0:
-            return render_template('results.html',reviews=reviews)
-        else:
-            inputstring = "https://www.flipkart.com/search?q="
-            outputstring = inputstring+searchString
-            source = requests.get(outputstring).text
-            flipkart_html  = BeautifulSoup(source,'lxml')
-            reviews = []
+        # if reviews.count() > 0:
+        #     return render_template('results.html',reviews=reviews)
+        # else:
+        inputstring = "https://www.flipkart.com/search?q="
+        outputstring = inputstring+searchString
+        source = requests.get(outputstring).text
+        flipkart_html  = BeautifulSoup(source,'lxml')
+        reviews = []
 
-            for article in flipkart_html.find_all("div", {"class": "_3O0U0u"}):
+        for article in flipkart_html.find_all("div", {"class": "_3O0U0u"}):
 
-                try:
-                    name = article.find("div",{"class": "_3wU53n"}).text
-                except :
-                    name = None
+            try:
+                name = article.find("div",{"class": "_3wU53n"}).text
+            except :
+                name = None
                     
-                try:
-                    cost = article.find("div",{"class": "_1vC4OE _2rQ-NK"}).text
-                except :
-                    cost = None
+            try:
+                cost = article.find("div",{"class": "_1vC4OE _2rQ-NK"}).text
+            except :
+                cost = None    
                         
-                try:
-                    desc = article.find("ul",{"class": "vFw0gD"}).text
-                except :
-                    desc = None
+            try:
+                desc = article.find("ul",{"class": "vFw0gD"}).text
+            except :
+                desc = None    
                 
 
-                mydict = { "Name": name, "Cost": cost, "Description": desc}
-                reviews.append(mydict)
-                x = collection.insert_one(mydict) 
+            mydict = { "Name": name, "Cost": cost, "Description": desc}
+            reviews.append(mydict)
+            # collection.insert_one(mydict)     
                 
 
         print(reviews)
@@ -66,5 +69,7 @@ def index():
     else:
         return render_template('index.html')        
 
+port = int(os.getenv("PORT"))
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0',port=port)
